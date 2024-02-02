049cfba28e91ba1ed4c92f6bd8dbfc1aebbc9518
Lenício Jr <lenicio.junior@hotmail.com>
2024-02-01 22:05:52 -0300

    Teste Select

--- cadastrarReceita.php
+++ cadastrarReceita.php
@@ -1,6 +1,14 @@
 <?php
+require "./config.php";
 
-$descricao = $_GET["descricao"];
-$valor = $_GET["valor"];
-$data_mvto = $_GET["data_mvto"];
-$categoria = $_GET["categoria"];
+// $descricao = $_GET["descricao"];
+// $valor = $_GET["valor"];
+// $data_mvto = $_GET["data_mvto"];
+// $categoria = $_GET["categoria"];
+
+$sql = "SELECT * FROM receitas";
+$sql = $pdo->prepare($sql);
+$sql->execute();
+
+echo "<pre>";
+print_r($sql->fetchAll(PDO::FETCH_ASSOC));
